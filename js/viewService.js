2edd6030162aaffffb1a0ec10ce08d08d94880eb
xxArisuhx <ariamela.trujillo@gmail.com>
2025-08-21 16:45:47 -0600

    feat(): change alt, seo, photos profile

--- js/viewService.js
+++ js/viewService.js
@@ -1,8 +1,9 @@
- document.querySelectorAll("li .titulo").forEach((titulo) => {
+// JS para la aparicion de los textos en la pagina de servicio
+
+document.querySelectorAll("li .titulo").forEach((titulo) => {
     titulo.addEventListener("click", () => {
       const li = titulo.parentElement;
       li.classList.toggle("active");
     });
   });
 
-  
